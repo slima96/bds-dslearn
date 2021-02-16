aa5191f2bfa4447dea5dfe1b4361fe4e90fb1e42
Sandro Lima <sandro.lima0016@gmail.com>
2021-02-16 12:31:01 -0300

    Backend project created

---
+++ backend/src/main/java/com/viatekbrasil/dslearn/DslearnApplication.java
@@ -0,0 +1,13 @@
+package com.viatekbrasil.dslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnApplication.class, args);
+	}
+
+}
