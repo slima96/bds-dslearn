2ab5f2458663af40c893401f2ca0d9b8d6fa2d77
Alexsandro Leão Lima <sandro.lima@viatek.local>
2021-02-25 18:14:03 -0300

    Validation, Exception handling

---
+++ backend/src/main/java/com/viatekbrasil/dslearn/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.viatekbrasil.dslearn.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.viatekbrasil.dslearn.dto.UserDTO;
+import com.viatekbrasil.dslearn.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@Autowired
+	private UserService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+		UserDTO dto = service.findById(id);
+		
+		return ResponseEntity.ok().body(dto);
+	}
+}
--- backend/src/main/java/com/viatekbrasil/dslearn/services/UserService.java
+++ backend/src/main/java/com/viatekbrasil/dslearn/services/UserService.java
@@ -1,5 +1,7 @@
 package com.viatekbrasil.dslearn.services;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,23 +9,33 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.viatekbrasil.dslearn.dto.UserDTO;
 import com.viatekbrasil.dslearn.entities.User;
 import com.viatekbrasil.dslearn.repositories.UserRepository;
+import com.viatekbrasil.dslearn.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService implements UserDetailsService {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
-	
+
 	@Autowired
 	private UserRepository repository;
+
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new UserDTO(entity);
+	}
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = repository.findByEmail(username);
-		
-		if (user ==null) {
+
+		if (user == null) {
 			logger.error("User not found:" + username);
 			throw new UsernameNotFoundException("Email not found");
 		}
