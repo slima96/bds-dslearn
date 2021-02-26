9e0e75c39e98b894cccd5f9027159874e187d49e
Alexsandro Leão Lima <sandro.lima@viatek.local>
2021-02-25 21:12:48 -0300

    Custom content for current user, JPQL query

--- backend/src/main/java/com/viatekbrasil/dslearn/repositories/NotificationRepository.java
+++ backend/src/main/java/com/viatekbrasil/dslearn/repositories/NotificationRepository.java
@@ -1,9 +1,18 @@
 package com.viatekbrasil.dslearn.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.viatekbrasil.dslearn.entities.Notification;
+import com.viatekbrasil.dslearn.entities.User;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
+	@Query("SELECT obj FROM Notification obj WHERE "
+			+ "(obj.user = :user) AND "
+			+ "(:unreadOnly = false OR obj.read = false) "
+			+ "ORDER BY obj.moment DESC")
+	Page<Notification> find(User user, boolean unreadOnly, Pageable pageable);
 }
---
+++ backend/src/main/java/com/viatekbrasil/dslearn/services/NotificationService.java
@@ -0,0 +1,27 @@
+package com.viatekbrasil.dslearn.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.viatekbrasil.dslearn.dto.NotificationDTO;
+import com.viatekbrasil.dslearn.entities.Notification;
+import com.viatekbrasil.dslearn.entities.User;
+import com.viatekbrasil.dslearn.repositories.NotificationRepository;
+
+@Service
+public class NotificationService {
+
+	@Autowired
+	private NotificationRepository repository;
+	
+	@Autowired
+	private AuthService authService;
+	
+	public Page<NotificationDTO> notificationsForCurrentUser(boolean unreadOnly, Pageable pageable) {
+		User user = authService.authenticated();
+		Page<Notification> page = repository.find(user, unreadOnly, pageable);
+		return page.map(x -> new NotificationDTO(x));
+	}
+}
